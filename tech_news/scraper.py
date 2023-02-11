b4747f24e2191ac4918f631d2bc461624d07a1a4
Paulo Ricardo <pauloricardosilvabarboza@outlook.com>
2023-02-11 13:58:50 -0300

    FEAT: Scrape news

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -26,9 +26,32 @@ def scrape_next_page_link(html_content):
     return selector.css("a.next.page-numbers::attr(href)").get()
 
 
-# Requisito 4
 def scrape_news(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(html_content)
+    raw_url = selector.css(".pk-share-buttons-facebook a::attr(href)").get()
+    url_index = raw_url.index("=") + 1
+    url = raw_url[url_index:]
+    title = selector.css(".entry-title::text").get().strip()
+    timestamp = selector.css(".meta-date::text").get()
+    writer = selector.css(".author a::text").get()
+    reading_time = selector.css(".meta-reading-time::text").re_first(r"\d+")
+    summary = (
+        selector.xpath('string(//div[@class="entry-content"]/p)')
+        .get()
+        .strip()
+    )
+    category = selector.css(".category-style .label::text").get()
+
+    news = {
+        "url": url,
+        "title": title,
+        "timestamp": timestamp,
+        "writer": writer,
+        "reading_time": int(reading_time),
+        "summary": summary,
+        "category": category,
+    }
+    return news
 
 
 # Requisito 5
